30a6698090be351375632213b8ecdc80a3e5670a
Jonatan Nascimento <quadrado4@gmail.com>
2023-04-18 17:07:07 -0300

    Adicinando resolução dos exercicios 1, 2 e 3 do curso Java

---
+++ src/Exercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int numero1 = sc.nextInt();
+		int numero2 = sc.nextInt();
+
+		if (numero1 % numero2 == 0 || numero2 % numero1 == 0) {
+			System.out.println("São multiplos");
+		} else {
+			System.out.println("Não são multiplos");
+		}
+
+		sc.close();
+	}
+
+}
